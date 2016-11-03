29bcb9b0a38fa93b107b015711b87c34bbc9cbdb
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-03 10:45:31 +0300

    [test_setup.py] Adds a BaseTest module Adds a Base test module and class providing a setup and teardown for a test database and environment variables for testing

---
+++ tests/test_setup.py
@@ -0,0 +1,20 @@
+import unittest
+
+from database.models import User, Bucketlist, Item
+from app import app, db
+
+class TestBaseCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tests/test.db'
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+if __name__ == "__main__":
+    unittest.main()
