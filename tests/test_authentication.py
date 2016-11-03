019360cef74f4f694ec19ce43b52d4b69c39f6c5
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-03 10:41:32 +0300

    [test_authentication.py] Adds test_register_user Adds the test_register_user function, and the imports required for it

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,12 +1,16 @@
-from authentication import authentication
-from unittest import mock, TestCase
+from authentication.authentication import Authentication
+from database.models import User
+from tests.test_setup import TestBaseCase
 
-class AuthenticationTest(TestCase):
+class AuthenticationTest(TestBaseCase):
 
-    def test_register_user():
-        pass
+    def test_register_user(self):
+        # register a new user and see they are added to the database
+        self.assertEqual(User.query.count(), 0)
+        Authentication.register_user({"username": "user", "password": "password123"})
+        self.assertEqual(User.query.count(), 1)
 
-    def test_login_user():
+    def test_login_user(self):
         pass 
 
 if __name__ == "__main__":
