491794ede0f991751d34ef52473217f5a4f898ba
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-03 10:48:08 +0300

    [bucketlist.py] Adds comment for methods

--- bucketlist/bucketlist.py
+++ bucketlist/bucketlist.py
@@ -1,25 +1,47 @@
 class Bucketlist(object):
+    """BucketList class 
+    
+    The bucket list class has methods for managing the bucketlists and items
+    Bucketlist enables one to create, list, return, update and delete 
+    bucketlists 
 
-    def create_bucketlist():
+    Each bucketlist has list items that can be created, updated and deleted
+    using the bucket list class
+    """
+
+    def create_bucketlist(name):
+        """creates a bucketlist using information sent using POST"""
         pass
 
     def list_bucketlists():
+        """lists all the bucketlists that are in the database"""
         pass
 
-    def get_bucketlist():
+    def get_bucketlist(identifier):
+        """returns a particular bucket list and its items"""
         pass
 
     def update_bucketlist():
+        """modifies information for a given bucketlist in the database"""
         pass
 
     def delete_bucketlist():
+        """deletes a particular bucketlist from the database"""
         pass
 
-    def create_item():
+    def search_bucketlist():
+        """searches for a bucketlist by using its name"""
+        pass
+
+    def create_item(name, bucketlist_id):
+        """creates an item in a particular bucketlist"""
         pass
 
     def update_item():
+        """updates a specified item in a particular bucketlist"""
         pass
 
     def delete_item():
+        """deletes a specified item in a particular bucketlist"""
         pass
+    
