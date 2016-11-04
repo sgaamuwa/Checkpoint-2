1cc5e0d7ef6595f20ad5a6022a28d47928b2e878
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-04 17:33:14 +0300

    [test_endpoints.py](Chore) Test refactoring Adds input data to the post and put statements for creating and updating bucketlists and items Adds docstrings Changes all single quotes (') to double quotes(")

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -4,98 +4,135 @@ from tests.test_setup import TestBaseCase
 
 
 class EndpointTests(TestBaseCase):
+    """Tests the endpoints in the application"""
 
     def test_valid_login(self):
+        """tests that valid login information enables one to login"""
         login_data = {"username": "Samuel", "password": "pass123"}
         response = self.app.post(
-            '/auth/login',
+            "/auth/login",
             data=login_data,
-            content_type='application/json')
+            content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
     def test_register_user(self):
-        login_data = {"username": "Samuel", "password": "incorrect"}
+        """tests that register responses with correct data"""
+        register_data = {"username": "Samuel", "password": "incorrect"}
         response = self.app.post(
-            '/auth/register', 
-            data=login_data, 
-            content_type='application/json'
+            "/auth/register", 
+            data=register_data, 
+            content_type="application/json"
             )
         self.assertEqual(response.status_code, 200)
 
     def test_create_bucketlist(self):
-        response = self.app.post('/bucketlists/')
+        """tests that a new bucketlist is created and the response received"""
+        bucketlist = {"name": "New Bucketlist"}
+        response = self.app.post(
+            "/bucketlists/",
+            data=bucketlist,
+            content_type="application/json"
+            )
         self.assertEqual(response.status_code, 200)
 
     def test_list_bucketlist(self):
-        response = self.app.get('/bucketlists/')
+        """tests that list bucketlist returns a list of bucketlists"""
+        response = self.app.get("/bucketlists/")
         self.assertEqual(response.status_code, 200)
 
     def test_searches_bucketlists(self):
+        """tests that a search by name returns the bucketlist item"""
         # With one that exists
-        response = self.app.get('/bucketlists?q=bucketlist')
+        response = self.app.get("/bucketlists?q=bucketlist")
         self.assertEqual(response.status_code, 200)
-        # With one that doesn't exist
-        response = self.app.get('/bucketlists?q=nolist')
+        # With one that doesn"t exist
+        response = self.app.get("/bucketlists?q=nolist")
         self.assertEqual(response.status_code, 404)
     
     def test_get_bucketlist(self):
+        """tests that a bucketlist is returned when its id is passed"""
         # test with a correct id
-        response = self.app.get('/bucketlists/1')
+        response = self.app.get("/bucketlists/1")
         self.assertEqual(response.status_code, 200)
         # test with a wrong id
-        response = self.app.get('/bucketlists/48')
+        response = self.app.get("/bucketlists/48")
         self.assertEqual(response.status_code, 404)
     
     def test_update_bucketlist(self):
+        """tests that bucketlists are updated in the system"""
         # test with a correct id
-        response = self.app.put('/bucketlists/1')
+        update_information = {"name": "new new name"}
+        response = self.app.put(
+            "/bucketlists/1",
+            data=update_information,
+            content_type="application/json"
+            )
         self.assertEqual(response.status_code, 200)
         # test with a wrong id
-        response = self.app.put('/bucketlists/43')
+        response = self.app.put("/bucketlists/43")
         self.assertEqual(response.status_code, 404)
 
     def test_delete_bucketlist(self):
+        """tests that bucketlists are deleted from the system"""
         # test with a correct id
-        response = self.app.delete('/bucketlists/1')
+        response = self.app.delete("/bucketlists/1")
         self.assertEqual(response.status_code, 200)
         # test with an incorrect id 
-        response = self.app.delete('/bucketlists/43')
+        response = self.app.delete("/bucketlists/43")
         self.assertEqual(response.status_code, 404)
 
     def test_create_item(self):
+        """tests that items are created in the system"""
         # test with a correct bucketlist id
-        response = self.app.post('/bucketlists/1/items/')
+        item = {"name": "item1"}
+        response = self.app.post(
+            "/bucketlists/1/items/"
+            data=item,
+            content_type="application/json")
         self.assertEqual(response.status_code, 200)
         # test with an incorrect bucketlist id
-        response = self.app.post('/bucketlists/43/items/')
+        response = self.app.post("/bucketlists/43/items/")
         self.assertEqual(response.status_code, 404)
 
     def test_update_item(self):
         # test with all correct ids
-        response = self.app.put('/bucketlists/1/items/1')
+        update_info = {"done": "true"}
+        response = self.app.put(
+            "/bucketlists/1/items/1",
+            data=update_info,
+            content_type="application/json")
         self.assertEqual(response.status_code, 200)
         # test with correct bucketlist id, incorrect item id
-        response = self.app.put('/bucketlists/1/items/43')
+        response = self.app.put(
+            "/bucketlists/1/items/43",
+            data=update_info,
+            content_type="application/json")
         self.assertEqual(response.status_code, 404)
         # test with incorrect bucketlist id, correct item id
-        response = self.app.put('/bucketlists/43/items/1')
+        response = self.app.put(
+            "/bucketlists/43/items/1",
+            data=update_info,
+            content_type="application/json")
         self.assertEqual(response.status_code, 404)
         # test with incorrect ids
-        response = self.app.put('/bucketlists/43/items/43')
+        response = self.app.put(
+            "/bucketlists/43/items/43",
+            data=update_info,
+            content_type="application/json")
         self.assertEqual(response.status_code, 404)
     
     def test_delete_item(self):
         # test with all correct ids
-        response = self.app.delete('/bucketlists/1/items/1')
+        response = self.app.delete("/bucketlists/1/items/1")
         self.assertEqual(response.status_code, 200)
         # test with correct bucketlist id, incorrect item id
-        response = self.app.delete('/bucketlists/1/items/43')
+        response = self.app.delete("/bucketlists/1/items/43")
         self.assertEqual(response.status_code, 404)
         # test with incorrect bucketlist id, correct item id
-        response = self.app.delete('/bucketlists/43/items/1')
+        response = self.app.delete("/bucketlists/43/items/1")
         self.assertEqual(response.status_code, 404)
         # test with incorrect ids
-        response = self.app.delete('/bucketlists/43/items/43')
+        response = self.app.delete("/bucketlists/43/items/43")
         self.assertEqual(response.status_code, 404)
 
 
