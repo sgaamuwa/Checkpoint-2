dd0dcc19710914b49541066158400294961053bd
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-07 12:11:28 +0300

    [test_bucketlist.py](Chore)Tests Refactoring Refactors the test for listing a bucketlist, getting a bucketlist, updating a bucketist, updating an item, deleting an item. Removing try and except statements and adding more asserts

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -18,32 +18,42 @@ class BucketlistTest(TestBaseCase):
     def test_list_bucketlists(self):
         """tests that all bucketlists in the system are listed"""
         # add to an empty bucketlist then list to see it is there
-        try:
-            self.assertEqual(len(BucketlistItem.list_bucketlists()), 1)
-        except:
-            return "no database data"
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
+        self.assertEqual(len(BucketlistItem.list_bucketlists()), 1)
+        # assert that there are now two in the list
+        BucketlistItem.create_bucketlist({"name": "blood"}, "samuel")
+        self.assertEqual(len(BucketlistItem.list_bucketlists()), 2)
+        # assert that there are now three in the list
+        BucketlistItem.create_bucketlist({"name": "hounds"}, "samuel")
+        self.assertEqual(len(BucketlistItem.list_bucketlists()), 3)
 
     def test_get_bucketlist(self):
         """tests that a particular bucketlist is retrieved"""
-        # try to retrieve from the things that are there
-        try:
-            self.assertEqual(BucketlistItem.get_bucketlist(1)["name"], "new")
-        except:
-            return "no database data"
+        # create new bucketlists 
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
+        BucketlistItem.create_bucketlist({"name": "blood"}, "samuel")
+        BucketlistItem.create_bucketlist({"name": "hounds"}, "samuel")
+        # assert that you can get the bucketlists
+        self.assertEqual(BucketlistItem.get_bucketlist(1)["name"], "new")
+        self.assertEqual(BucketlistItem.get_bucketlist(2)["name"], "blood")
+        self.assertEqual(BucketlistItem.get_bucketlist(3)["name"], "hounds")
+        # assert that you can get by name
+        self.assertEqual(BucketlistItem.get_bucketlist({"name": "new"})["name"], "new")
+        self.assertEqual(BucketlistItem.get_bucketlist({"name": "blood"})["name"], "blood")
+        self.assertEqual(BucketlistItem.get_bucketlist({"name": "hounds"})["name"], "hounds")
 
     def test_update_bucketlist(self):
         """tests that a particular bucketlist can be updated"""
-        # add something to the list and try to update it
-        data = {"name": "new name"}
-        bucketlist_id = 1
-        try:
-            bucketlist = Bucketlist.query.filter_by(id=bucketlist_id).first()
-            self.assertEqual(bucketlist.name, "name")
-            BucketlistItem.update_bucketlist(item_id, data)
-            item = Bucketlist.query.filter(id=bucketlist_id).first()
-            self.assertEqual(bucketlist.name, "new name")
-        except:
-            return "no database data"
+        # create a bucketlist
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
+        bucketlist = Bucketlist.query.filter_by(id=1).first()
+        # assert that the name is the same 
+        self.assertEqual(bucketlist.name, "new")
+        # update bucketlist with a new name
+        BucketlistItem.update_bucketlist({"name": "new name"}, 1)
+        item = Bucketlist.query.filter_by(id=1).first()
+        # assert that the name is the updated name
+        self.assertEqual(bucketlist.name, "new name")
 
     def test_delete_bucketlist(self):
         """tests that a specified bucketlist can be deleted from the database"""
@@ -52,41 +62,44 @@ class BucketlistTest(TestBaseCase):
         # assert that there are now two bucketlists
         self.assertEqual(Bucketlist.query.count(), 1)
         # delete the new bucketlists
-        BucketlistItem.delete_bucketlist({"id": 1})
+        BucketlistItem.delete_bucketlist(1)
         # assert that it is now one bucket list
         self.assertEqual(Bucketlist.query.count(), 0)
 
     def test_create_item(self):
         """tests that a specified item is created"""
+        # create bucketlist for item
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
         # assert that there are no items in the database
         self.assertEqual(Item.query.count(), 0)
         # create one item
-        BucketlistItem.create_item("name", 1)
+        BucketlistItem.create_item({"name": "item1"}, 1)
         # assert that there is now one item in the database
         self.assertEqual(Item.query.count(), 1)
 
     def test_udpate_item(self):
         """tests that a particular bucketlist item can be updated"""
-        # determine the update data
-        data = {"name": "new name"}
-        item_id = 1
-        try:
-            # try if there is data in the database
-            item = Item.query.filter_by(id=item_id).first()
-            self.assertEqual(item.name, "name")
-            BucketlistItem.update_item(item_id, data)
-            item = Item.query.filter(id=item_id).first()
-            self.assertEqual(item.name, "new name")
-        except:
-            # else return that there is no data 
-            return "no database data"
+        # create bucketlist and Item
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
+        BucketlistItem.create_item({"name": "item1"}, 1)
+        # assert that the item is not done
+        item = Item.query.filter_by(id=1).first()
+        self.assertEqual(item.done, False)
+        # update the item
+        BucketlistItem.update_item({"done": True}, 1, 1)
+        # assert the done has changed
+        item = Item.query.filter_by(id=1).first()
+        self.assertEqual(item.done, True)
 
     def test_delete_item(self):
         """tests that a particular bucketlist item can be deleted"""
+        # create a bucketlist and a bucketlist item
+        BucketlistItem.create_bucketlist({"name": "new"}, "samuel")
+        BucketlistItem.create_item({"name": "item1"}, 1)
         # assert that there is one item in the database
         self.assertEqual(Item.query.count(), 1)
         # delete that item
-        BucketlistItem.delete_item(1)
+        BucketlistItem.delete_item(1, 1)
         # assert that there is no item in the database
         self.assertEqual(Item.query.count(), 0)
 
