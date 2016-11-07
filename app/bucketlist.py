113cc4f3ca18d15e75224e31a3a50654d746fd16
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-07 11:55:11 +0300

    [bucketlist.py](Feature) Bucketlists and Items Adds implemention for getting a single bucketlist from the database. Adds implementation for updating a single bucketlist  in the database. Adds a return message for deleting a bucketlist and changes the method parameter from a dictionary to an id integer. Adds implementation for creating an item for a bucketlist and adding to the database. Adds implementation for updating an item in the database. Adds implementation for deleting an item from the database. Removes the search bucketlist method. Refactors lines 23, 48, 49 and 50 to bind them to particular objects.

--- app/bucketlist.py
+++ app/bucketlist.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from app.models import Bucketlist, Item
-from app.app import db
+from models import Bucketlist, Item
+from app import db
 
 
 class BucketlistItem(object):
@@ -20,7 +20,7 @@ class BucketlistItem(object):
             name=data["name"],
             date_created=datetime.now(),
             date_modified=datetime.now(),
-            created_by=user
+            created_by=user.username
         )
         db.session.add(bucketlist)
         db.session.commit()
@@ -45,9 +45,9 @@ class BucketlistItem(object):
                 item_dict = {
                     "id": item.id,
                     "name": item.name,
-                    "date_created": date_created,
-                    "date_modified": date_modified,
-                    "done": done
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "done": item.done
                 }
                 item_list.append(item_dict)
             bucketlist_dict = {
@@ -62,35 +62,88 @@ class BucketlistItem(object):
         return bucketlist_list
 
 
-    def get_bucketlist(identifier):
+    def get_bucketlist(data):
         """returns a particular bucket list and its items"""
-        pass
-
-    def update_bucketlist():
+        # if "id" in data.keys():
+        bucketlist = Bucketlist.query.filter_by(id=data).first()
+        # elif "name" in data.keys():
+        #     bucketlist = Bucketlist.query.filter_by(name=data["name"]).first()
+        item_list = []
+        items = Item.query.filter_by(bucketlist=bucketlist.id).all()
+        for item in items:
+            item_dict = {
+                "id": item.id,
+                "name": item.name,
+                "date_created": date_created,
+                "date_modified": date_modified,
+                "done": done
+            }
+            item_list.append(item_dict)
+        bucketlist_dict = {
+                "id": bucketlist.id,
+                "name": bucketlist.name,
+                "items": item_list,
+                "date_created": bucketlist.date_created,
+                "date_modified": bucketlist.date_modified,
+                "created_by": bucketlist.created_by
+            }
+        return bucketlist_dict
+        
+    def update_bucketlist(data, id):
         """modifies information for a given bucketlist in the database"""
-        pass
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        bucketlist.name = data["name"]
+        bucketlist.date_modified = datetime.now()
+        db.session.commit()
 
-    def delete_bucketlist(data):
+    def delete_bucketlist(id):
         """deletes a particular bucketlist from the database"""
-        bucketlist = Bucketlist.query.filter_by(id=data["id"]).first()
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
         db.session.delete(bucketlist)
         db.session.commit()
         db.session.close()
+        return {"message": "Bucketlist ID:{} deleted".format(id)}
 
-    def search_bucketlist():
-        """searches for a bucketlist by using its name"""
-
-        pass
-
-    def create_item(name, bucketlist_id):
+    def create_item(data, bucketlist_id):
         """creates an item in a particular bucketlist"""
-        pass
+        bucketlist = Bucketlist.query.filter_by(id=bucketlist_id).first()
+        item = Item(
+            name=data["name"],
+            date_created=datetime.now(),
+            date_modified=datetime.now(),
+            done=False,
+            bucketlist=bucketlist.id
+        )
+        bucketlist.date_modified = datetime.now()
+        db.session.add(item)
+        db.session.commit()
+        new_entry = {
+            "id": item.id,
+            "name": item.name,
+            "date_created": item.date_created,
+            "date_modified": item.date_modified,
+            "done": item.done,
+            "bucketlist": bucketlist.name
+        }
+        db.session.close()
+        return new_entry
 
-    def update_item():
+    def update_item(data):
         """updates a specified item in a particular bucketlist"""
-        pass
+        bucketlist = Bucketlist.query.filter_by(id=data["bucketlist"]).first()
+        item = Item.query.filter_by(id=data["id"], bucketlist=data["bucketlist"]).first()
+        item.done = data["done"]
+        item.date_modified = datetime.now()
+        bucketlist.date_modified = datetime.now()
+        db.session.commit()
 
-    def delete_item():
+    def delete_item(id, bucketlist_id):
         """deletes a specified item in a particular bucketlist"""
-        pass
+        item = Item.query.filter_by(id=id, bucketlist=bucketlist_id).first()
+        db.session.delete(item)
+        db.session.commit()
+        db.session.close()
+        return {
+            "message": "Item ID:{} deleted from Bucketlist ID:{}".format(id, bucketlist_id)
+            }
     
