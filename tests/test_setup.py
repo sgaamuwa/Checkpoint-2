3589d82f71a3e1c63637b90bcd04630902471fc3
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-08 16:27:22 +0300

    [test_setup.py](Chore) Test Authentication Adds an authenticated user for the tests to access restricted urls. Adds a sample bucketlist and sample list item to the test database. Adds json import and refactors app import to from resource ...

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,7 +1,8 @@
 import unittest
+import json
 
 from app.models import User, Bucketlist, Item
-from app.app import app, db
+from resource import app, db
 
 
 class TestBaseCase(unittest.TestCase):
@@ -12,6 +13,47 @@ class TestBaseCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../tests/test.db'
         self.app = app.test_client()
         db.create_all()
+
+        
+        data = {"username": "samuel", "password": "pass123"}
+        # register the User
+        reg_resp = self.app.post(
+            "/auth/register",
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        # log in the user
+        login_resp = self.app.post(
+            "/auth/login",
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        # get a token
+        headers = {
+            'Authorization': 'Basic ' + 'c2FtdWVsOnBhc3MxMjM='
+        }
+
+        response = self.app.get("/token", headers=headers)
+        token = json.loads(response.get_data().decode())["token"]
+
+        self.headers = {
+            "Authorization": "Bearer " + token
+        }
+        # create a test bucketlist
+        bucketlist = {"name": "New Bucketlist"}
+        buck_resp = self.app.post(
+            "/bucketlists/",
+            data=json.dumps(bucketlist),
+            content_type="application/json",
+            headers=self.headers
+            )
+        # create a test Item
+        item = {"name": "item1"}
+        response = self.app.post(
+            "/bucketlists/1/items/",
+            data=json.dumps(item),
+            content_type="application/json",
+            headers=self.headers)
 
     def tearDown(self):
         db.session.remove()
