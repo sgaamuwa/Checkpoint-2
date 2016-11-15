ff1f41d79313008483b5114b3d62a3530ef9f6db
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-15 15:13:58 +0300

    [app.py, .coverge, README.md, requirements.txt](Chore) README Adds README badges(README.md) adds configuration foro sqlalchemy track modificatins to app.py Adds coveralls to requirements file.

--- app/app.py
+++ app/app.py
@@ -5,5 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bucketlist.db"
 app.config["SECRET_KEY"] = "kinggaamuwasamuel"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
