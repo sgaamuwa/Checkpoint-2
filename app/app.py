5bbc17c6752b9f9b47a8803be2138c372c3a96b9
andela-sgaamuwa <samuel.gaamuwa@andela.com>
2016-11-17 16:19:07 +0300

    [app.py] Removes the secret key.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bucketlist.db"
-app.config["SECRET_KEY"] = "kinggaamuwasamuel"
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
